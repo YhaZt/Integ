edcf21a44dd17b358f9114c663b7d931cd652534
Carpel Sweet Dreams B. Lanto <sc1.csdlanto2021@joysistvi.edu.ph>
2022-12-17 15:55:20 +0800

    frontend
    new component

--- Integ/frontend/src/app/app.module.ts
+++ Integ/frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './include/header/header.component';
+import { FooterComponent } from './include/footer/footer.component';
+import { SidebarComponent } from './include/sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetComponent } from './reset/reset.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
