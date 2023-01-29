89e8b7f477a64576d844fec0d4d63605189fea4d
Castillo Angelo <82747051+AngeloCast@users.noreply.github.com>
2023-01-29 18:54:29 +0800

    angelo
    angelo push

--- Integ/backend/core/urls.py
+++ Integ/backend/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from core import views
-
+from .views import RefreshAPIView, LogoutAPIView
 
 urlpatterns = [
     path('register', views.RegisterAPIView.as_view()),
